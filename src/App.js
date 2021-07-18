1830eaf8174a1ea42332cba7534b577c05478811
Mateus Lanuce <mateuslanuce@gmail.com>
2021-07-18 13:54:58 -0300

    logica de negocios finalizada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [ email, setEmail] = useState('');
+
+  function cadastrarEmail() {
+    localStorage.setItem('emailCadastro', JSON.stringify(email));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>E-commerce</h1>
+
+      <input type="email" placeholder="Digite seu email"  value={email} onChange={event => setEmail(event.target.value)} />
+      <button type="button" onClick={cadastrarEmail}>Pesquisar</button>
     </div>
   );
 }
